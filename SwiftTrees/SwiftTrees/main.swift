93b22eda5bcd9d7ee37b7693a946e01ec1aa2596
Dave Spina <dspina79@gmail.com>
2021-05-01 20:47:12 -0400

    Basic tree definition

--- SwiftTrees/SwiftTrees/main.swift
+++ SwiftTrees/SwiftTrees/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Node<Value> {
+    var value: Value
+    private(set) var children: [Node]
+    
+    mutating func addChildNode(child: Node) {
+        children.append(child)
+    }
+    
+    init(_ value: Value) {
+        self.value = value
+        children = []
+    }
+    
+    init(_ value: Value, children: [Node]) {
+        self.value = value
+        self.children = children
+    }
+    
+}
 
